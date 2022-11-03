1512e80350da569bd7716a17ce1501cf02758fa6
Willian M. Vargas <willianm.vargas@gmail.com>
2022-11-03 20:03:14 -0300

    requisito 02 feito

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
 import Loading from '../components/Loading';
 
@@ -7,6 +8,7 @@ export default class Login extends Component {
     inputName: '',
     disable: true,
     loading: false,
+    redirect: false,
   };
 
   buttonEnable = () => {
@@ -17,22 +19,27 @@ export default class Login extends Component {
     });
   };
 
-  creatUser = async (event) => {
+  creatingUser = async (event) => {
     const { inputName } = this.state;
     event.preventDefault();
-    await this.onChangeValue('loading', true);
+    this.onChangeValue({ target: { name: 'loading', value: true } });
     await createUser({ name: inputName });
-    await this.onChangeValue('loading', false);
+    this.setState({
+      loading: false,
+      inputName: '',
+      redirect: true,
+    });
   };
 
   onChangeValue = ({ target: { name, value } }) => {
+    console.log('mudou');
     this.setState({
       [name]: value,
     }, this.buttonEnable);
   };
 
   render() {
-    const { inputName, disable, loading } = this.state;
+    const { inputName, disable, loading, redirect } = this.state;
     return (
       <div data-testid="page-login">
         <form action="">
@@ -46,12 +53,13 @@ export default class Login extends Component {
           />
           <button
             type="submit"
-            onClick={ this.creatUser }
+            onClick={ this.creatingUser }
             disabled={ disable }
             data-testid="login-submit-button"
           >
             Entrar
           </button>
+          { redirect && <Redirect to="/search" />}
         </form>
       </div>
     );
