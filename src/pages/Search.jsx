dca316047226ce4bb1f3829987166ec9ae1436a6
Willian M. Vargas <willianm.vargas@gmail.com>
2022-11-03 21:27:01 -0300

    requisito 5 feito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,42 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    search: '',
+    disable: true,
+  };
+
+  buttonEnable = () => {
+    const { search } = this.state;
+    const MIN_NAME_LENGTH = 2;
+    this.setState({
+      disable: search.length < MIN_NAME_LENGTH,
+    });
+  };
+
+  onChangeValue = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    }, this.buttonEnable);
+  };
+
   render() {
+    const { disable } = this.state;
+
     return (
       <div data-testid="page-search">
-        Search
         <Header />
+        <form action="">
+          <input
+            type="text"
+            name="search"
+            onChange={ this.onChangeValue }
+            data-testid="search-artist-input"
+          />
+          <button type="submit" disabled={ disable } data-testid="search-artist-button">
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
