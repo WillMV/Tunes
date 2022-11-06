4b954e7f05c2a9ade95d544f19e000d705ffc7f3
Willian M. Vargas <willianm.vargas@gmail.com>
2022-11-06 17:02:37 -0300

    requisito 11 feito

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,23 +1,48 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Loading from './Loading';
-import { getFavoriteSongs, addSong } from '../services/favoriteSongsAPI';
+import { getFavoriteSongs, addSong, removeSong } from '../services/favoriteSongsAPI';
 
 export default class MusicCard extends Component {
   state = {
     isLoadin: false,
     isFav: false,
+    favList: [],
   };
 
   async componentDidMount() {
     const { music: { trackId } } = this.props;
     this.setState({ isLoadin: true });
     const fetch = await getFavoriteSongs();
-    const hadFav = fetch.find((musicFav) => musicFav.trackId === trackId);
-    this.setState({ isLoadin: false, isFav: hadFav });
+    const hadFav = fetch.some((musicFav) => musicFav.trackId === trackId);
+    this.setState({ isLoadin: false, isFav: hadFav, favList: fetch });
   }
 
-  saveFavTrack = async ({ target: { checked } }) => {
+  controlFavTrack = async (event) => {
+    const { favList } = this.state;
+    const { music } = this.props;
+    const inFavList = favList
+      .some((favItem) => favItem.trackId === music.trackId);
+    console.log(inFavList);
+    console.log(favList);
+
+    if (inFavList) {
+      await this.removeFavTrack(event);
+    } else {
+      await this.addFavTrack(event);
+    }
+    const newFavs = await getFavoriteSongs;
+    this.setState({ favList: newFavs });
+  };
+
+  removeFavTrack = async ({ target: { checked } }) => {
+    const { music } = this.props;
+    this.setState({ isLoadin: true, isFav: checked });
+    await removeSong(music);
+    this.setState({ isLoadin: false });
+  };
+
+  addFavTrack = async ({ target: { checked } }) => {
     const { music } = this.props;
     this.setState({ isLoadin: true, isFav: checked });
     await addSong(music);
@@ -41,7 +66,7 @@ export default class MusicCard extends Component {
             data-testid={ `checkbox-music-${trackId}` }
             type="checkbox"
             checked={ isFav }
-            onClick={ this.saveFavTrack }
+            onChange={ this.controlFavTrack }
             id={ trackId }
           />
           Favorita
